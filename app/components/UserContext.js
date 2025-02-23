1d80db0a6b55c52c6b460221be96905a2a2fba11
abel <abeloabate01@gmail.com>
2025-02-23 19:05:53 +0300

    mi

--- app/components/UserContext.js
+++ app/components/UserContext.js
@@ -15,7 +15,7 @@ export const UserProvider = ({ children }) => {
         father: 'none',
         userId: undefined, //userId: undefined,
         recentDisabled: "666,9090",
-        profit: null,
+        profit: 0,
         profile: "https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png"
 
     });
